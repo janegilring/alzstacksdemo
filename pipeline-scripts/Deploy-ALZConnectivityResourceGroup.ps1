4288f494ee14673d87c6fedacdc0aae5a8360e7c
Jan Egil Ring <janegilring@microsoft.com>
2023-06-01 12:17:56 +0000

    Updated to leverage New-AzManagementGroupDeploymentStack
    Signed-off-by: Jan Egil Ring <janegilring@microsoft.com>

--- pipeline-scripts/Deploy-ALZConnectivityResourceGroup.ps1
+++ pipeline-scripts/Deploy-ALZConnectivityResourceGroup.ps1
@@ -9,9 +9,10 @@ param (
   [String]$TemplateFile = "upstream-releases\$($env:UPSTREAM_RELEASE_VERSION)\infra-as-code\bicep\modules\resourceGroup\resourceGroup.bicep",
 
   [Parameter()]
-  [String]$TemplateParameterFile = "config\custom-parameters\resourceGroupConnectivity.parameters.all.json"
+  [String]$TemplateParameterFile = 'config\custom-parameters\resourceGroupConnectivity.parameters.all.json'
 )
 
+<#
 # Parameters necessary for deployment
 $inputObject = @{
   DeploymentName        = 'alz-ConnectivityRGDeploy-{0}' -f ( -join (Get-Date -Format 'yyyyMMddTHHMMssffffZ')[0..63])
@@ -24,3 +25,19 @@ $inputObject = @{
 Select-AzSubscription -SubscriptionId $ConnectivitySubscriptionId
 
 New-AzSubscriptionDeployment @inputObject
+#>
+
+$inputObject = @{
+  Name                     = 'ALZ-Connectivity}'
+  ManagementGroupId        = 'alz-platform-connectivity'
+  Location                 = $Location
+  TemplateFile             = $TemplateFile
+  TemplateParameterFile    = $TemplateParameterFile
+  DeploymentSubscriptionId = $ConnectivitySubscriptionId
+  DeleteAll                = $true
+  Tag                      = @{Environment = 'Demo' }
+  Verbose                  = $true
+  DenySettingsMode         = 'DenyDelete'
+}
+
+New-AzManagementGroupDeploymentStack @inputObject
