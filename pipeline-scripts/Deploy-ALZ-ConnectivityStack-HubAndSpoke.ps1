39a0f51ae737f085117b05c16559447f0668d334
Jan Egil Ring <janegilring@microsoft.com>
2023-06-05 10:53:33 +0000

    Updated paths
    Signed-off-by: Jan Egil Ring <janegilring@microsoft.com>

--- pipeline-scripts/Deploy-ALZ-ConnectivityStack-HubAndSpoke.ps1
+++ pipeline-scripts/Deploy-ALZ-ConnectivityStack-HubAndSpoke.ps1
@@ -6,8 +6,8 @@ param (
   [String]$ConnectivitySubscriptionId = "$($env:CONNECTIVITY_SUBSCRIPTION_ID)",
 
   [Parameter()]
-  [String]$TemplateFile = "upstream-releases\$($env:UPSTREAM_RELEASE_VERSION)\infra-as-code\bicep\modules\resourceGroup\resourceGroup.bicep",
-
+  [String]$TemplateFile = "upstream-releases\$($env:UPSTREAM_RELEASE_VERSION)\infra-as-code\bicep\modules\orchestration\resourceGroup.bicep",
+  connectivityDeployment/connectivityDeployment.bicep
   [Parameter()]
   [String]$TemplateParameterFile = 'config\custom-parameters\resourceGroupConnectivity.parameters.all.json'
 )
