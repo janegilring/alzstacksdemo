8e9c41423e19f3c1f4e9cb3ce4aa6e2864314787
Jan Egil Ring <janegilring@microsoft.com>
2023-06-10 16:41:33 +0000

    Add deploy SPNs object ID to DenySettingsExcludedPrincipals
    Signed-off-by: Jan Egil Ring <janegilring@microsoft.com>

--- pipeline-scripts/02-Deploy-ALZLoggingAndSentinelStack.ps1
+++ pipeline-scripts/02-Deploy-ALZLoggingAndSentinelStack.ps1
@@ -17,17 +17,18 @@ param (
 )
 
 $inputObject = @{
-  Name                     = 'ALZ-Logging'
-  ManagementGroupId        = 'fd91810c-57b4-43e3-b513-c2a81e8d6a27' #'Tenant Root Group'
-  Location                 = $Location
-  TemplateFile             = $TemplateFile
-  TemplateParameterFile    = $TemplateParameterFile
-  DeploymentSubscriptionId = $ManagementSubscriptionId
-  DeleteAll                = $true
-  Tag                      = @{Environment = 'Demo' }
-  Verbose                  = $true
-  DenySettingsMode         = 'DenyWriteAndDelete'
-  Force                    = $true
+  Name                           = 'ALZ-Logging'
+  ManagementGroupId              = 'fd91810c-57b4-43e3-b513-c2a81e8d6a27' #'Tenant Root Group'
+  Location                       = $Location
+  TemplateFile                   = $TemplateFile
+  TemplateParameterFile          = $TemplateParameterFile
+  DeploymentSubscriptionId       = $ManagementSubscriptionId
+  DeleteAll                      = $true
+  Tag                            = @{Environment = 'Demo' }
+  Verbose                        = $true
+  DenySettingsMode               = 'DenyWriteAndDelete'
+  DenySettingsExcludedPrincipals = '2702bead-8908-4edd-a184-d8e3232fa109'
+  Force                          = $true
 }
 
 $StackExists = Get-AzManagementGroupDeploymentStack -Name $inputObject.Name -ManagementGroupId $inputObject.ManagementGroupId -ErrorAction SilentlyContinue
